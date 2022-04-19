a0f8526139ccd0706900b8695dcfd4daab154f49
juanPaPortugal <pabloayoroa23@gmail.com>
2022-04-19 14:23:06 -0300

    agregando listas de forma dinamica en el formulario

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ const btnAgregar = document.querySelector("[data-form-btn]");
 
 const createTask = (e) => {
     e.preventDefault();
+    //inputForm guarda el input -->caja de escritura a traves del data-...input
     const inputForm = document.querySelector("[data-form-input]")
-    console.log(inputForm.value);   
+    const value = inputForm.value; // guarda el contenido del input
+    const list = document.querySelector("[data-list]")
+
+    const task = document.createElement("li"); //;creando un li lista en  task
+    task.classList.add("card");//agrega una clase mediante ".classlist.add"
+    inputForm.value = "";
+
+    //creando contenido html mediante backsticks
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+    console.log(content);   
+    task.innerHTML = content; //se agrega al li que esta guardado en la consante task, contenido html mediante ".innerHTML"
+    /* agregando un child con .appendchild */
+    list.appendChild(task)
 }
 
 btnAgregar.addEventListener("click", createTask);
