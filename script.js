b7347c52aa940566dc4cd2244ef513553dcfc6cc
juanPaPortugal <pabloayoroa23@gmail.com>
2022-04-19 23:14:22 -0300

    agregando IIFE,toggle y target al form

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
-const btnAgregar = document.querySelector("[data-form-btn]");
+(() => {
+  // inmediately invoked function expression IIFE
 
-const createTask = (e) => {
+  const btnAgregar = document.querySelector("[data-form-btn]");
+
+  const createTask = (e) => {
     e.preventDefault();
     //inputForm guarda el input -->caja de escritura a traves del data-...input
-    const inputForm = document.querySelector("[data-form-input]")
+    const inputForm = document.querySelector("[data-form-input]");
     const value = inputForm.value; // guarda el contenido del input
-    const list = document.querySelector("[data-list]")
-
-    const task = document.createElement("li"); //;creando un li lista en  task
-    task.classList.add("card");//agrega una clase mediante ".classlist.add"
+    const list = document.querySelector("[data-list]");
     inputForm.value = "";
 
-    //creando contenido html mediante backsticks
-    const content = ` <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`
-    console.log(content);   
-    task.innerHTML = content; //se agrega al li que esta guardado en la consante task, contenido html mediante ".innerHTML"
-    /* agregando un child con .appendchild */
-    list.appendChild(task)
-}
+    //creando li, div, span
+    const task = document.createElement("li"); //;
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
 
-btnAgregar.addEventListener("click", createTask);
+    //adhiriendo classes mediante .classlist.add()
+    task.classList.add("card");
+    titleTask.innerText = value;
+    titleTask.classList.add("task");
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+
+    //agregando un child al parent
+    task.appendChild(taskContent);
+    list.appendChild(task);
+    const content = ` 
+  
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    //task.innerHTML = content; //se agrega al li que esta guardado en la consante task, contenido html mediante ".innerHTML"
+    /* agregando un child con .appendchild */
+  };
+
+  btnAgregar.addEventListener("click", createTask);
+
+  const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", CompleteTask);
+    return i;
+  };
+
+  //funcion para animar el chek de la lista
+  //togle hace que quite o ponga una clase
+  const CompleteTask = (event) => {
+    const element = event.target; // target es una propiedad de event y selecciona el elemento clikeado
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+  };
+})(); // inmediately invoked function expression IIFE
+//se usa para proteger nuestro codigo y no sea accecible desde consola al usuario
